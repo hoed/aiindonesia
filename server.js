f0004fac1b3be116d1a69b1ed33b697cb60cc589
hoed <hoedhud@gmail>
2024-06-27 05:26:50 +0700

    Initialize project using Create React App

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post('/gemini', async (req, res) => {
     const model = genAI.getGenerativeModel({ model: gemini-1.5-flash})
 
     const chat = model startChat({
-        history : req.body.history,
+        history : req.body.history
 
     })
     const msg = req.body.message
